eb77824f3394bd43ef3c4707bc4daee6eab4c748
lolleri200 <45740831+lolleri200@users.noreply.github.com>
2020-11-23 09:18:23 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: false,
   firebase: {
     apiKey: "AIzaSyCBxqCumEOP1GR53xUqCuwCOIqDpv6g_dw",
-    authDomain: "ng-simple-workout-d626e.firebaseapp.com",
-    databaseURL: "https://ng-simple-workout-d626e.firebaseio.com",
-    projectId: "ng-fitness-tracker-d626e",
-    storageBucket: "ng-simple-workout-d626e.appspot.com",
-    messagingSenderId: "867032762734"
+    authDomain: "ng-WorkoutApp-d626e.firebaseapp.com",
+    databaseURL: "https://ng-WorkoutApp-d656e.firebaseio.com",
+    projectId: "ng-WorkoutApp-d656e",
+    storageBucket: "ng-WorkoutApp-d656e.appspot.com",
+    messagingSenderId: "867032737934"
   }
 };
