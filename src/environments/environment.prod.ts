727d3f307ba0d49886c6cc7dd7690ff24febcc4f
lolleri200 <45740831+lolleri200@users.noreply.github.com>
2020-11-23 09:21:55 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   firebase: {
     apiKey: "AIzaSyCBxqCumEOP1GR53xUqCuwCOIqDpv6g_dw",
     authDomain: "ng-d656e-d656e.firebaseapp.com",
-    databaseURL: "https://ng-d656e-d626e.firebaseio.com",
-    projectId: "ng-d656e-d626e",
-    storageBucket: "ng-d656e-d626e.appspot.com",
+    databaseURL: "https://ng-d656e-d656e.firebaseio.com",
+    projectId: "ng-d656e-d666e",
+    storageBucket: "ng-d656e-d656e.appspot.com",
     messagingSenderId: "867093752734"
   }
 };
