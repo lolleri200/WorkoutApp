64357e99046e601c86fd85c01b0bc907747b0cc5
lolleri200 <45740831+lolleri200@users.noreply.github.com>
2020-11-23 09:24:07 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: "AIzaSyCBxqCumEOP1GR53xUqCuwCOIqDpv6g_dw",
-    authDomain: "ng-d656e-d656e.firebaseapp.com",
-    databaseURL: "https://ng-d656e-d656e.firebaseio.com",
-    projectId: "ng-d656e-d666e",
-    storageBucket: "ng-d656e-d656e.appspot.com",
-    messagingSenderId: "867093752734"
+    authDomain: "ng-WorkoutApp-d656e.firebaseapp.com",
+    databaseURL: "https://ng-WorkoutApp-d656e.firebaseio.com",
+    projectId: "ng-WorkoutApp-d656e",
+    storageBucket: "https://ng-WorkoutApp-d656e.firebaseio.com",
+    messagingSenderId: "867032737934"
   }
 };
